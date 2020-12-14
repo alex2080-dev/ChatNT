5811a4cfcf2a9d34046635478ae7ae1669be51e7
alex.kir <alex.kirienko2080@gmail.com>
2020-12-14 11:56:44 +0600

    Java2 ДЗ Урок 5

--- src/sample/ThreadsTiming.java
+++ src/sample/ThreadsTiming.java
@@ -43,7 +43,6 @@ public static void TwoThreads() {
                 }}, "Поток1");
 
 
-
     Thread thread2 = new Thread (() -> {
         for (int i = 0; i < a2.length; i++) {
             a2[i] = (float) (a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
