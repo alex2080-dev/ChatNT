716b7b7f816c5d4cb3634d5c95930ba2c119493f
alex.kir <alex.kirienko2080@gmail.com>
2021-01-06 22:12:25 +0600

    Java3 ДЗ Урок 1

--- src/Java3/TestBoxing.java
+++ src/Java3/TestBoxing.java
@@ -15,14 +15,15 @@ public class TestBoxing {
 
         Box<Apple> box1 = new Box<>();
         Box<Orange> box2 = new Box<>();
-        Box<?> box3 = new Box<>();
+        Box<Orange> box3 = new Box<>();
 
-        box1.addFruit(a1);box1.addFruit(a2);box1.addFruit(a3);box1.addFruit(a4);box1.addFruit(a5);
-        box2.addFruit(o1);box2.addFruit(o2);box2.addFruit(o3);
+       box1.addFruit(a1);box1.addFruit(a2);box1.addFruit(a3);box1.addFruit(a4);box1.addFruit(a5);
+       box2.addFruit(o1);box2.addFruit(o2);box2.addFruit(o3);
 
-        System.out.println("Вес этих коробок совпадает - " + box1.compare(box2));
+        System.out.println("Вес этих коробок совпадает - " + box1.compare(box2)+ " (" + box1.getWeight() + "&" + box2.getWeight()+")");
 
-        box3.addBox(box1);
+        box3.addBox(box2);
+        box3.addBox(box2);
 
         System.out.println(box3.getlist());
     }
